40d71e96afd7b3a3b536ff0a0f1668b307fbf5b0
Jessica Ward <jessicawardiom@gmail.com>
2024-12-03 18:02:58 +0000

    Day 3a completed

---
+++ day03/day3a.py
@@ -0,0 +1,21 @@
+import re
+
+def read_file(filename: str) -> str:
+    with open(filename) as f:
+        return f.read()
+    
+def get_all_valid_instructions(instructions: str) -> list[str]:
+    return re.findall(r'mul\(\d{1,3},\d{1,3}\)', instructions)
+
+def get_left_and_right_operands(valid_instruction: str) -> tuple[int, int]:
+    comma_seperated_operands = valid_instruction.replace("mul(", "").replace(')', '')
+    split_operands = comma_seperated_operands.split(',')
+    return split_operands[0], split_operands[1]
+
+instructions = read_file("input.txt")
+matches = get_all_valid_instructions(instructions)
+pairs = [get_left_and_right_operands(match) for match in matches]
+multiplied_pairs = [int(pair[0]) * int(pair[1]) for pair in pairs]
+sum_of_multiplied_pairs = sum(multiplied_pairs)
+
+print(sum_of_multiplied_pairs)
